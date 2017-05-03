0da715429069b0b9bada1fbb854eb89333fceb3b
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-05-03 13:50:26 +0200

    Configs fro deployment.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-  PORT: process.env.PORT | 3000,
-  DATA_URL: "mongodb://localhost:27017/voter"
+  PORT: process.env.PORT || 3000,
+  DATA_URL: process.env.DATA_URL || "mongodb://localhost:27017/voter"
 };
 
 export default config;
